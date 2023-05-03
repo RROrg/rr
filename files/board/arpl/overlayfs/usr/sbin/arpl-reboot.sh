25bcdf18d2ead4679ebde264f65314b27d8b0b1b
Ing <wjz304@qq.com>
2023-05-04 01:45:03 +0800

    add find grub path, typo

--- files/board/arpl/overlayfs/usr/sbin/arpl-reboot.sh
+++ files/board/arpl/overlayfs/usr/sbin/arpl-reboot.sh
@@ -8,5 +8,8 @@ function use() {
 [ -z "${1}" ] && use
 [ "${1}" != "junior" -a "${1}" != "config" ] && use
 echo "Rebooting to ${1} mode"
-grub-editenv /mnt/p1/boot/grub/grubenv set next_entry="${1}"
+GRUBPATH="$(dirname $(find /mnt/p1/ -name grub.cfg | head -1))"
+ENVFILE="${GRUBPATH}/grubenv"
+[ ! -f "${ENVFILE}" ] && grub-editenv ${ENVFILE} create
+grub-editenv ${ENVFILE} set next_entry="${1}"
 reboot
