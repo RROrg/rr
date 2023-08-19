19d142c2d836b64cae5352db282616b34513c16a
Ing <wjz304@qq.com>
2023-08-19 21:11:28 +0800

    format

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 #
 # Copyright (C) 2022 Ing <https://github.com/wjz304>
-# 
+#
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-
 
 GRUB=${1:-"grub-2.06"}
 BIOS=${2:-"i386-pc i386-efi x86_64-efi"}
@@ -14,8 +13,7 @@ curl -#kLO https://ftp.gnu.org/gnu/grub/${GRUB}.tar.gz
 tar -zxvf ${GRUB}.tar.gz
 
 pushd ${GRUB}
-for B in ${BIOS}
-do
+for B in ${BIOS}; do
   b=${B}
   b=(${b//-/ })
   echo "Make ${b[@]} ..."
@@ -29,13 +27,12 @@ do
 done
 popd
 
-
 rm -f grub.img
 dd if=/dev/zero of=grub.img bs=1M seek=1024 count=0
 echo -e "n\np\n1\n\n+50M\nn\np\n2\n\n+50M\nn\np\n3\n\n\na\n1\nw\nq\n" | fdisk grub.img
 fdisk -l grub.img
 
-LOOPX=`sudo losetup -f`
+LOOPX=$(sudo losetup -f)
 sudo losetup -P ${LOOPX} grub.img
 sudo mkdosfs -F32 -n ARPL1 ${LOOPX}p1
 sudo mkfs.ext2 -F -L ARPL2 ${LOOPX}p2
@@ -47,19 +44,18 @@ sudo mount ${LOOPX}p1 ARPL1
 
 sudo mkdir -p ARPL1/EFI
 sudo mkdir -p ARPL1/boot/grub
-cat > device.map <<EOF
+cat >device.map <<EOF
 (hd0)   ${LOOPX}
 EOF
 sudo mv device.map ARPL1/boot/grub/device.map
 
-for B in ${BIOS}
-do
+for B in ${BIOS}; do
   args=""
   args+=" ${LOOPX} --target=${B} --no-floppy --recheck --grub-mkdevicemap=ARPL1/boot/grub/device.map --boot-directory=ARPL1/boot"
   if [[ "${B}" == *"efi" ]]; then
-      args+=" --efi-directory=ARPL1 --removable --no-nvram"
+    args+=" --efi-directory=ARPL1 --removable --no-nvram"
   else
-      args+=" --root-directory=ARPL1"
+    args+=" --root-directory=ARPL1"
   fi
   sudo ${GRUB}/${B}/grub-install ${args}
 done
