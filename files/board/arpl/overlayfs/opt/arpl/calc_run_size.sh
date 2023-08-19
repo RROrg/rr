ef7a915034d301b11c5b60d8c09c9a35bff1286d
Ing <wjz304@qq.com>
2023-08-19 21:12:39 +0800

    sort models and modify the display of disks

--- files/board/arpl/overlayfs/opt/arpl/calc_run_size.sh
+++ files/board/arpl/overlayfs/opt/arpl/calc_run_size.sh
@@ -8,9 +8,9 @@
 
 NUM='\([0-9a-fA-F]*[ \t]*\)'
 OUT=$(sed -n 's/^[ \t0-9]*.b[sr][sk][ \t]*'"${NUM}${NUM}${NUM}${NUM}"'.*/\1\4/p')
-if [ -z "$OUT" ] ; then
-	echo "Never found .bss or .brk file offset" >&2
-	exit 1
+if [ -z "$OUT" ]; then
+  echo "Never found .bss or .brk file offset" >&2
+  exit 1
 fi
 
 OUT=$(echo ${OUT# })
@@ -22,20 +22,20 @@ sizeB=$(printf "%d" 0x${OUT%% *})
 OUT=${OUT#* }
 offsetB=$(printf "%d" 0x${OUT%% *})
 
-run_size=$(( ${offsetA} + ${sizeA} + ${sizeB} ))
+run_size=$((${offsetA} + ${sizeA} + ${sizeB}))
 
 # BFD linker shows the same file offset in ELF.
-if [ "${offsetA}" -ne "${offsetB}" ] ; then
-	# Gold linker shows them as consecutive.
-	endB=$(( ${offsetB} + ${sizeB} ))
-	if [ "$endB" != "$run_size" ] ; then
-		printf "sizeA: 0x%x\n" ${sizeA} >&2
-		printf "offsetA: 0x%x\n" ${offsetA} >&2
-		printf "sizeB: 0x%x\n" ${sizeB} >&2
-		printf "offsetB: 0x%x\n" ${offsetB} >&2
-		echo ".bss and .brk are non-contiguous" >&2
-		exit 1
-	fi
+if [ "${offsetA}" -ne "${offsetB}" ]; then
+  # Gold linker shows them as consecutive.
+  endB=$((${offsetB} + ${sizeB}))
+  if [ "$endB" != "$run_size" ]; then
+    printf "sizeA: 0x%x\n" ${sizeA} >&2
+    printf "offsetA: 0x%x\n" ${offsetA} >&2
+    printf "sizeB: 0x%x\n" ${sizeB} >&2
+    printf "offsetB: 0x%x\n" ${offsetB} >&2
+    echo ".bss and .brk are non-contiguous" >&2
+    exit 1
+  fi
 fi
 
 printf "%d\n" ${run_size}
