334c186c1f3cd77f50573e2860f720a693920366
Ing <wjz304@qq.com>
2023-08-20 21:18:15 +0800

    format

--- files/board/arpl/overlayfs/opt/arpl/include/functions.sh
+++ files/board/arpl/overlayfs/opt/arpl/include/functions.sh
@@ -60,20 +60,20 @@ function dieLog() {
 ###############################################################################
 # Generate a number with 6 digits from 1 to 30000
 function random() {
-  printf "%06d" $(($RANDOM % 30000 + 1))
+  printf "%06d" $((${RANDOM} % 30000 + 1))
 }
 
 ###############################################################################
 # Generate a hexa number from 0x00 to 0xFF
 function randomhex() {
-  printf "&02X" "$(($RANDOM % 255 + 1))"
+  printf "&02X" "$((${RANDOM} % 255 + 1))"
 }
 
 ###############################################################################
 # Generate a random letter
 function generateRandomLetter() {
   for i in A B C D E F G H J K L M N P Q R S T V W X Y Z; do
-    echo $i
+    echo ${i}
   done | sort -R | tail -1
 }
 
@@ -81,7 +81,7 @@ function generateRandomLetter() {
 # Generate a random digit (0-9A-Z)
 function generateRandomValue() {
   for i in 0 1 2 3 4 5 6 7 8 9 A B C D E F G H J K L M N P Q R S T V W X Y Z; do
-    echo $i
+    echo ${i}
   done | sort -R | tail -1
 }
 
@@ -118,7 +118,7 @@ function validateSerial() {
     echo 0
     return
   fi
-  echo ${PREFIX} | grep -q ${S}
+  echo "${PREFIX}" | grep -q "${S}"
   if [ $? -eq 1 ]; then
     echo 0
     return
@@ -162,19 +162,19 @@ function _get_conf_kv() {
 # 3 - path
 function _set_conf_kv() {
   # Delete
-  if [ -z "$2" ]; then
-    sed -i "$3" -e "s/^$1=.*$//"
+  if [ -z "${2}" ]; then
+    sed -i "${3}" -e "s/^${1}=.*$//"
     return $?
   fi
 
   # Replace
-  if grep -q "^$1=" "$3"; then
-    sed -i "$3" -e "s\"^$1=.*\"$1=\\\"$2\\\"\""
+  if grep -q "^${1}=" "${3}"; then
+    sed -i "${3}" -e "s\"^${1}=.*\"${1}=\\\"${2}\\\"\""
     return $?
   fi
 
   # Add if doesn't exist
-  echo "$1=\"$2\"" >>$3
+  echo "${1}=\"${2}\"" >>"${3}"
 }
 
 ###############################################################################
@@ -184,7 +184,7 @@ function findAndMountDSMRoot() {
   [ $(mount | grep -i "${DSMROOT_PATH}" | wc -l) -gt 0 ] && return 0
   dsmrootdisk="$(blkid /dev/sd* | grep -i raid | awk '{print $1 " " $4}' | grep UUID | grep sd[a-z]1 | head -1 | awk -F ":" '{print $1}')"
   [ -z "${dsmrootdisk}" ] && return -1
-  [ $(mount | grep -i "${DSMROOT_PATH}" | wc -l) -eq 0 ] && mount -t ext4 $dsmrootdisk "${DSMROOT_PATH}"
+  [ $(mount | grep -i "${DSMROOT_PATH}" | wc -l) -eq 0 ] && mount -t ext4 "${dsmrootdisk}" "${DSMROOT_PATH}"
   if [ $(mount | grep -i "${DSMROOT_PATH}" | wc -l) -eq 0 ]; then
     echo "Failed to mount"
     return -1
