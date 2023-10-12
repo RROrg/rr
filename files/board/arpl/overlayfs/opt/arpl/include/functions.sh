0f3e6eca1c71c01c85367af96e912dbdd068906b
Ing <wjz304@qq.com>
2023-10-12 13:54:47 +0800

    fix something

--- files/board/arpl/overlayfs/opt/arpl/include/functions.sh
+++ files/board/arpl/overlayfs/opt/arpl/include/functions.sh
@@ -206,6 +206,73 @@ function _get_fastest() {
 }
 
 ###############################################################################
+# sort netif name
+# @1 -mac1,mac2,mac3...
+function _sort_netif() {
+  ETHLIST=""
+  ETHX=$(ls /sys/class/net/ | grep eth) # real network cards list
+  for ETH in ${ETHX}; do
+    MAC="$(cat /sys/class/net/${ETH}/address | sed 's/://g' | tr '[:upper:]' '[:lower:]')"
+    BUS=$(ethtool -i ${ETH} | grep bus-info | awk '{print $2}')
+    ETHLIST="${ETHLIST}${BUS} ${MAC} ${ETH}\n"
+  done
+
+  if [ -n "${1}" ]; then
+    MACS=$(echo "${1}" | sed 's/://g' | tr '[:upper:]' '[:lower:]' | tr ',' ' ')
+    ETHLISTTMPC=""
+    ETHLISTTMPF=""
+
+    for MACX in ${MACS}; do
+      ETHLISTTMPC="${ETHLISTTMPC}$(echo -e "${ETHLIST}" | grep "${MACX}")\n"
+    done
+
+    while read -r BUS MAC ETH; do
+      [ -z "${MAC}" ] && continue
+      if echo "${MACS}" | grep -q "${MAC}"; then continue; fi
+      ETHLISTTMPF="${ETHLISTTMPF}${BUS} ${MAC} ${ETH}\n"
+    done <<EOF
+$(echo -e ${ETHLIST} | sort)
+EOF
+    ETHLIST="${ETHLISTTMPC}${ETHLISTTMPF}"
+  else
+    ETHLIST="$(echo -e "${ETHLIST}" | sort)"
+  fi
+  ETHLIST="$(echo -e "${ETHLIST}" | grep -v '^$')"
+
+  echo -e "${ETHLIST}" > /tmp/ethlist
+  # cat /tmp/ethlist
+
+  # sort
+  IDX=0
+  while true; do
+    # cat /tmp/ethlist 
+    [ ${IDX} -ge $(wc -l < /tmp/ethlist) ] && break
+    ETH=$(cat /tmp/ethlist | sed -n "$((${IDX} + 1))p" | awk '{print $3}')
+    # echo "ETH: ${ETH}"
+    if [ -n "${ETH}" ] && [ ! "${ETH}" = "eth${IDX}" ]; then
+      # echo "change ${ETH} <=> eth${IDX}"
+      ip link set dev eth${IDX} down
+      ip link set dev ${ETH} down
+      sleep 1
+      ip link set dev eth${IDX} name tmp
+      ip link set dev ${ETH} name eth${IDX}
+      ip link set dev tmp name ${ETH}
+      sleep 1
+      ip link set dev eth${IDX} up
+      ip link set dev ${ETH} up
+      sleep 1
+      sed -i "s/eth${IDX}/tmp/" /tmp/ethlist
+      sed -i "s/${ETH}/eth${IDX}/" /tmp/ethlist
+      sed -i "s/tmp/${ETH}/" /tmp/ethlist
+      sleep 1
+    fi
+    IDX=$((${IDX} + 1))
+  done
+
+  rm -f /tmp/ethlist
+}
+
+###############################################################################
 # Find and mount the DSM root filesystem
 # (based on pocopico's TCRP code)
 function findAndMountDSMRoot() {
--- guide.md
+++ guide.md
@@ -186,23 +186,28 @@
 * log:
   ```
   # 驱动相关
-  lsmod                                            # 查看已加载驱动  
-  ls -ld /sys/class/net/*/device/driver            # 查看已加载网卡和对应驱动  
+  lsmod                                            # 查看已加载驱动
+  ls -ld /sys/class/net/*/device/driver            # 查看已加载网卡和对应驱动
   cat /sys/class/net/*/address                     # 查看已加载网卡的MAC地址
 
-  # 磁盘相关   
-  fdisk -l                                         # 查看硬盘信息 
-  ls /sys/block/                                   # 查看块设备  
-  ls /sys/block/sd*                                # 查看识别的 sata 硬盘 (非设备树(dtb)的型号)    
-  ls /sys/block/sata*                              # 查看识别的 sata 硬盘  (设备树(dtb)的型号)  
-  ls /sys/block/nvme*                              # 查看识别的 nvme 硬盘  
+  # 磁盘相关
+  fdisk -l                                         # 查看硬盘信息
+  lspci -d ::106                                   # 查看 ATA 控制器
+  lspci -d ::107                                   # 查看 HBA 控制器
+  ls -l /sys/class/scsi_host                       # 查看硬盘 host 信息
+  ls /sys/block/                                   # 查看块设备
+  ls /sys/block/sd*                                # 查看识别的 sata 硬盘 (非设备树(dtb)的型号)
+  ls /sys/block/sata*                              # 查看识别的 sata 硬盘  (设备树(dtb)的型号)
+  ls /sys/block/nvme*                              # 查看识别的 nvme 硬盘
   cat /sys/block/sd*/device/syno_block_info        # 查看识别的 sata 硬盘挂载点 (非设备树(dtb)的型号)  
-  cat /sys/block/sata*/device/syno_block_info      # 查看识别的 sata 硬盘挂载点 (设备树(dtb)的型号)  
-  cat /sys/block/nvme*/device/syno_block_info      # 查看识别的 nvme 硬盘挂载点  
+  cat /sys/block/sata*/device/syno_block_info      # 查看识别的 sata 硬盘挂载点 (设备树(dtb)的型号)
+  cat /sys/block/nvme*/device/syno_block_info      # 查看识别的 nvme 硬盘挂载点
 
+  # 服务相关
   systemctl                                        # 查看服务  
+  netstat -tunlp                                   # 查看端口  
 
-  # 日志相关   
+  # 日志相关
   dmesg                                            # 内核日志
   cat /proc/cmdlime                                # 引导参数
   cat /var/log/linuxrc.syno.log                    # 引导态下启动日志
