39127d060d3bab4802328aa69cb9e66e989f66d2
Ing <wjz304@qq.com>
2024-01-13 23:21:46 +0800

    fix i18n

--- files/initrd/opt/rr/include/i18n.sh
+++ files/initrd/opt/rr/include/i18n.sh
@@ -3,13 +3,20 @@
 if [ -d "/usr/share/locale" ]; then
   if [ $(ls ${WORK_PATH}/lang/*.mo 2>/dev/null | wc -l) -gt 0 ]; then
     for F in $(ls ${WORK_PATH}/lang/*.mo); do
-      mkdir -p "/usr/share/locale/$(basename "${F}" .mo)/LC_MESSAGES"
-      install "${F}" "/usr/share/locale/$(basename "${F}" .mo)/LC_MESSAGES/rr.mo"
+      LANG="$(basename "${F}" .mo)"
+      LOCALE="${LANG%%_*}"
+      if [ -d "/usr/share/locale/${LANG}" ] || [ ! -d "/usr/share/locale/${LOCALE}" ]; then
+        mkdir -p "/usr/share/locale/${LANG}/LC_MESSAGES"
+        install "${F}" "/usr/share/locale/${LANG}/LC_MESSAGES/rr.mo"
+      else
+        mkdir -p "/usr/share/locale/${LOCALE}/LC_MESSAGES"
+        install "${F}" "/usr/share/locale/${LOCALE}/LC_MESSAGES/rr.mo"
+      fi
     done
   fi
 
   if [ -f ${PART1_PATH}/.locale ]; then
-    export LANG="$(cat ${PART1_PATH}/.locale)"
+    export LC_ALL="$(cat ${PART1_PATH}/.locale)"
   fi
 
   alias TEXT='gettext "rr"'
