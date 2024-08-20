6f1105a043e4607b99e6f3a15d65bc970966722b
Ing <wjz304@qq.com>
2024-08-20 15:43:29 +0800

    chore: Update timezone selection logic and set timezone based on user choice

--- files/initrd/opt/rr/include/i18n.sh
+++ files/initrd/opt/rr/include/i18n.sh
@@ -1,16 +1,19 @@
 [ -z "${WORK_PATH}" -o ! -d "${WORK_PATH}/include" ] && WORK_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/../" >/dev/null 2>&1 && pwd)"
 
 if type gettext >/dev/null 2>&1; then
-  if [ -d "${WORK_PATH}/lang" ]; then
-    export TEXTDOMAINDIR="${WORK_PATH}/lang"
-  fi
-  if [ -f "${PART1_PATH}/.locale" ]; then
-    export LC_ALL="$(cat ${PART1_PATH}/.locale)"
-  fi
-
   alias TEXT='gettext "rr"'
   shopt -s expand_aliases
 else
   alias TEXT='echo'
   shopt -s expand_aliases
+fi
+if [ -d "${WORK_PATH}/lang" ]; then
+  export TEXTDOMAINDIR="${WORK_PATH}/lang"
+fi
+if [ -f "${PART1_PATH}/.locale" ]; then
+  export LC_ALL="$(cat ${PART1_PATH}/.locale)"
+fi
+if [ -f "${PART1_PATH}/.timezone" ]; then
+  TIMEZONE="$(cat ${PART1_PATH}/.timezone)"
+  ln -sf "/usr/share/zoneinfo/right/${TIMEZONE}" /etc/localtime
 fi
