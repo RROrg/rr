6c76070b387648944987143b6a57bbbb5b0e6927
Ing <wjz304@qq.com>
2025-04-18 03:04:35 +0800

    优化 curl 命令，增加连接超时设置

--- files/initrd/opt/rr/include/functions.sh
+++ files/initrd/opt/rr/include/functions.sh
@@ -224,7 +224,7 @@ function _get_fastest() {
     done
   else
     for I in "$@"; do
-      speed=$(curl -skL -w '%{time_total}' "${I}" -o /dev/null)
+      speed=$(curl -skL -m 10 --connect-timeout 10 -w '%{time_total}' "${I}" -o /dev/null)
       speed=$(awk "BEGIN {print (${speed:-0.999} * 1000)}")
       speedlist+="${I} ${speed:-999}\n" # Assign default value 999 if speed is empty
     done
